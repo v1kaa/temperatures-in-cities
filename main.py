8e90d341491099be33023e84c86c972053c7c08f
V1kaa <dziaduhvi@gmail.com>
2024-02-04 21:58:33 +0100

    1

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+def get_city_data():
+    while True:
+        name = input("Name of the city: ")
+        api_url = f'https://api.api-ninjas.com/v1/city?name={name}'
+        response = requests.get(api_url, headers={'X-Api-Key': 'YOUR-API-KEY'}) #here must be your api_key
+        if response.status_code == requests.codes.ok:
+            try:
+                data = response.json()
+                if data:
+                    return data[0]
+                else:
+                    print("There is no city with this name, or something went wrong. Try again.")
+            except json.decoder.JSONDecodeError:
+                print("Error decoding JSON content. Try again.")
+        else:
+            print(f"Request failed with status code: {response.status_code}. Try again.")
+
+def kelvin_to_celsius(kelvin):
+    return round(kelvin - 273.15, 2)
+
+def get_weather_data(lat, lon, appid):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {'lat': lat, 'lon': lon, 'appid': appid}
+    response = requests.get(url, params=params).json()
+    return response['main']['temp']
+
+
+city_data = get_city_data()
+lat, lon = city_data['latitude'], city_data['longitude']
+appid = '51e0b498db42548b353953e7e3fee2f6'
+
+temperature = get_weather_data(lat, lon, appid)
+
+print(f"\nTemperature in {city_data['name'].upper()} is about: {kelvin_to_celsius(temperature)} ℃")
